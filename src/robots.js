6ba697bd69c4e7cf2bc083e854b5671724e0f22c
Agnibha Pal <pal.agnibha97@gmail.com>
2021-05-29 15:25:23 +0530

    latest commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Agnibha Pal',
+        username: 'Pugnibha',
+        email: 'pugnibha@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Ananya Pandey',
+        username: 'AnAnEy',
+        email: 'ananya@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Sabrina Howladar',
+        username: 'Sab',
+        email: 'sabby@hotmail.com'
+    },
+    {
+        id: 4,
+        name: 'Sayan Bandukwala',
+        username: 'Sayan',
+        email: 'dhiskyaon@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Udbhas Gaanwala',
+        username: 'Potol',
+        email: 'potolkumar@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Krishnasis Computerwala',
+        username: 'Krishnasys',
+        email: 'kisna@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Debayan Tabalchi',
+        username: 'Debu',
+        email: 'sltabalchi@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Rajarshi Uswala',
+        username: 'Ninja',
+        email: 'ninjaraju@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Souhardya CRwala',
+        username: 'Champion',
+        email: 'kaunbaatkarrahahai@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Rai Nyakamo',
+        username: 'Nayika',
+        email: 'dryrai@gmail.com'
+    }
+]
